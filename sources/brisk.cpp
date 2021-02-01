8b87d07b966186ec97686452a201034e8bb20737
Mini <njoer16@student.sdu.dk>
2021-02-01 14:12:27 +0100

    Updated Brisk.cpp
    Fixed the filtering of matches, now works great on the freiburg dataset from deepfactors.
    Code that helped fix match filtering from: https://gist.github.com/Jinqiang/8133713

--- sources/brisk.cpp
+++ sources/brisk.cpp
@@ -12,8 +12,8 @@ int main() {
 
     //Setup images and vectors for keypoints and descriptors.
     cv::Mat img1, img2;
-    img1 = cv::imread("/home/nicklas/Pictures/and.png", 0);
-    img2 = cv::imread("/home/nicklas/Pictures/and.png", 0);
+    img1 = cv::imread("/home/nicklas/Desktop/img1.png", 0);
+    img2 = cv::imread("/home/nicklas/Desktop/img2.png", 0);
     std::vector<cv::KeyPoint> keypointsimg1, keypointsimg2;
     cv::Mat descriptorsimg1, descriptorsimg2;
 
@@ -32,24 +32,42 @@ int main() {
     //Setup matcher
     cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::BRUTEFORCE_HAMMING);
     std::vector<cv::DMatch> matches;
+    std::vector<cv::DMatch> good_matches;
+    std::vector<cv::Point2f> sel_img1, sel_img2;
 
     //Match
     matcher->match(descriptorsimg1, descriptorsimg2, matches);
 
-    //INDSÆT MATCH FILTER
-    /*float filter_thresh = 0.8f;
-    std::vector<cv::DMatch> filtered_matches;
-    for(int i=0; i<matches.size(); i++){
-        if (matches[i].distance < filter_thresh * matches[i].distance)
-        {
-            filtered_matches.push_back(matches[i]);
-        }
-    }*/
+    //Match filtering
+    double max_dist, min_dist;
+    for (int i =0; i < matches.size();i++)
+    {
+        if(max_dist<matches[i].distance) max_dist = matches[i].distance;
+        if(min_dist>matches[i].distance) min_dist = matches[i].distance;
+    }
+    min_dist = min_dist + (max_dist- min_dist)* 0.3;
+    for (int i=0;i<matches.size();i++)
+    {
+        if( matches[i].distance< min_dist)
+            good_matches.push_back(matches[i]);
+    }
 
-    //Draw and show matches
+    //Sort keypoints in images so only relevant keypoints are remaining
+    for(int i=0;i<good_matches.size();i++){
+        sel_img1.push_back(keypointsimg1[good_matches[i].queryIdx].pt);
+        sel_img2.push_back(keypointsimg2[good_matches[i].trainIdx].pt);
+    }
+
+    std::cout << "total matches: " << matches.size() << std::endl;
+    std::cout << "good matches: " << good_matches.size() << std::endl;
+    std::cout << "sel_img1 size: " << sel_img1.size() << std::endl;
+    std::cout << "sel_img2 size: " << sel_img1.size() << std::endl;
+
+    //Draw matches
     cv::Mat matchImg;
-    cv::drawMatches(img1, keypointsimg1, img2, keypointsimg2, matches,
-                    matchImg, cv::Scalar::all(-1), cv::Scalar::all(-1), std::vector<char>(),cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+    cv::drawMatches(img1, keypointsimg1, img2, keypointsimg2, good_matches,
+                    matchImg, cv::Scalar::all(-1), cv::Scalar::all(-1), std::vector<char>(),
+                    cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
     cv::imshow("Matches", matchImg);
 
     //Detect on vid stream
